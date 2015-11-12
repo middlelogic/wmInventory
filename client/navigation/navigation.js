44d414b47ba9f235cdb29e281d4fc6f865d149fc
Tim Cooper <timothy.cooper@shea.ca>
2015-11-12 00:25:31 -0500

    Collapse navbar on click - bootstrap fix

--- client/navigation/navigation.js
+++ client/navigation/navigation.js
@@ -7,9 +7,20 @@ if(Meteor.isClient) {
       // Logout Current User
       Meteor.logout();
 
+      // Toggle navbar collapse
+      $('.navbar-toggle').click();
+
       // Re-direct to Sign-in
       Router.go('/signin');
     }
   });
+
+  Template.navigation.rendered = function() {
+
+    $('.navbar ul.nav li a').on('click', function(){
+      $('.navbar-toggle').click();
+    });
+
+  }
 
 }
